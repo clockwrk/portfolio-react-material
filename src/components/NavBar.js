88a440dd69f645dfcb8bcd30671fffb08ea7315e
Jose Renteria <JoseRenteria.sf@outlook.com>
2018-04-15 15:11:09 -0700

    added react starter and started routing application

---
+++ src/components/NavBar.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand, NavbarNav, NavbarToggler, Collapse, NavItem, NavLink, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'mdbreact';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Home from './Home.js'
+
+class NavBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            collapse: false,
+            isWideEnough: false,
+            dropdownOpen: false
+        };
+    this.onClick = this.onClick.bind(this);
+    this.toggle = this.toggle.bind(this);
+    }
+
+    onClick(){
+        this.setState({
+            collapse: !this.state.collapse,
+        });
+    }
+
+    toggle() {
+        this.setState({
+            dropdownOpen: !this.state.dropdownOpen
+        });
+    }
+
+    render() {
+        return (
+            <Router>
+            <div>
+                <Navbar color="indigo" dark expand="md" scrolling>
+
+                    { !this.state.isWideEnough && <NavbarToggler onClick = { this.onClick } />}
+                    <Collapse isOpen = { this.state.collapse } navbar>
+                        <NavbarNav left>
+                          <NavItem active>
+                              <NavLink to="/">Home</NavLink>
+                          </NavItem>
+                          <NavItem>
+                              <NavLink to="/about">About</NavLink>
+                          </NavItem>
+                          <NavItem>
+                              <NavLink to="/contact">Contact</NavLink>
+                          </NavItem>
+                          <NavItem>
+                              <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+                              <DropdownToggle nav caret>Projects</DropdownToggle>
+                              <DropdownMenu>
+                                  <DropdownItem>
+                                    <Link to="/projects">Projects</Link>
+                                  </DropdownItem>
+                                  <DropdownItem>
+                                    <Link to="/projects/one">Project 1</Link>
+                                  </DropdownItem>
+                                  <DropdownItem>
+                                    <Link to="/projects/two">Project 2</Link>
+                                  </DropdownItem>
+                                  <DropdownItem>
+                                    <Link to="/projects/three">Project 3</Link>
+                                  </DropdownItem>
+                              </DropdownMenu>
+                              </Dropdown>
+                          </NavItem>
+                        </NavbarNav>
+                    </Collapse>
+                </Navbar>
+                <hr />
+                <Route exact path="/" component={Home} />
+                </div>
+            </Router>
+        );
+    }
+}
+
+export default NavBar;
